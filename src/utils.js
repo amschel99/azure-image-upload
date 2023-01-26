e8acb658156c79c97238c9c5c44ff4edf038e250
amschel99 <6290.2020@students.ku.ac.ke>
2023-01-26 12:46:41 +0300

    changed the SAS url

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@
 import { BlobServiceClient} from "@azure/storage-blob"
 
 
-const sasToken="YOUR_SAS_TOKEN"
+const sasToken="https://motaautombiles.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2030-01-31T22:52:01Z&st=2023-01-23T14:52:01Z&spr=https,http&sig=GQjBdYiru%2BSr9DG6HbKtAggXnM4A5rp63Lub4GGuxRY%3D"
 const blobServiceClient = new BlobServiceClient(
  `${sasToken}`
 );
